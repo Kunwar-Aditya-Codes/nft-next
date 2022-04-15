fafc21d5c64226d0b457cae6094a7f28de131f38
Kunwar Aditya <adityakunwar0147@gmail.com>
2022-04-16 05:25:22 +0530

    sanity

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -3,6 +3,11 @@ import { useAddress, useDisconnect, useMetamask } from '@thirdweb-dev/react'
 
 const NftDropPage = () => {
   // Authentication
+  const connectToMetamask = useMetamask()
+  const address = useAddress()
+  const disconnect = useDisconnect()
+
+  console.log(address)
 
   return (
     <div className="flex h-screen flex-col lg:grid lg:grid-cols-10">
@@ -26,25 +31,34 @@ const NftDropPage = () => {
       </div>
 
       {/*Right*/}
-      <div className="flex flex-1 flex-col p-12 lg:col-span-6">
+      <div className="flex flex-1 flex-col p-11 lg:col-span-6">
         <header className="flex items-center justify-between">
-          <h1 className="w-52 cursor-pointer text-xl font-extralight lg:text-2xl ">
-            <span className="font-extrabold underline decoration-fuchsia-500/50 underline-offset-1">
-              Cyber
+          <h1 className="w-52 cursor-pointer text-xl font-extralight tracking-wider lg:text-2xl">
+            <span className="font-extrabold text-fuchsia-500 underline decoration-fuchsia-500 underline-offset-1">
+              CYBER
             </span>{' '}
-            Nft MarketPlace
+            NFT MARKETPLACE
           </h1>
-          <button className="rounded-full border-2  bg-gradient-to-b from-fuchsia-500 to-pink-500 px-4 py-2 font-bold text-white">
-            Sign In
+          <button
+            onClick={() => (address ? disconnect() : connectToMetamask())}
+            className="rounded-full border-2  bg-gradient-to-b from-fuchsia-500 to-pink-500 px-4 py-2 font-bold text-white"
+          >
+            {address ? 'Sign Out' : 'Sign In'}
           </button>
         </header>
         <hr className="my-2  rounded-full border" />
+        {address && (
+          <p className="mt-4 text-center text-blue-800 lg:text-lg">
+            You are logged in with wallet {address.substring(0, 5)}...
+            {address.substring(address.length - 5)}
+          </p>
+        )}
         <div className="my-8 flex flex-1 flex-col items-center space-y-7 text-center lg:justify-center">
           <img src="/images/hero.png" alt="" className=" lg:w-2/3" />
           <h1 className=" text-3xl font-medium lg:text-5xl">
             Code with Cyber Punks!
           </h1>
-          <p className="text-green-500">13/25 NFT's claimed!</p>
+          <p className="text-green-500 lg:text-lg">13/25 NFT's claimed!</p>
         </div>
 
         <button className="h-16 w-full  rounded-full bg-gradient-to-b from-fuchsia-500 to-pink-500 text-lg font-bold text-white">
